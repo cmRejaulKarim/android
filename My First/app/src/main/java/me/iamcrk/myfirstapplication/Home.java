18f3752e0c091a0937e858b93bc1dce9e8730a4e
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-09-03 14:09:36 +0600

    ddddd

--- My First/app/src/main/java/me/iamcrk/myfirstapplication/Home.java
+++ My First/app/src/main/java/me/iamcrk/myfirstapplication/Home.java
@@ -1,6 +1,9 @@
 package me.iamcrk.myfirstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
 
+    protected Button btnSubject, btnRound;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +24,27 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
 
+        btnSubject = findViewById(R.id.btnSubject);
+        btnRound = findViewById(R.id.btnRound);
+
+
+        btnSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),Subject.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnRound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),Round.class);
+
+                startActivity(intent);
+            }
+        });
 
     }
 }
