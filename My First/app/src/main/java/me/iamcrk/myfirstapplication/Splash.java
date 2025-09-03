37066d8c154eb0aafc5769a92490c071859e7ead
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-09-03 13:01:43 +0600

    Home 24 from 37

--- My First/app/src/main/java/me/iamcrk/myfirstapplication/Splash.java
+++ My First/app/src/main/java/me/iamcrk/myfirstapplication/Splash.java
@@ -1,6 +1,11 @@
 package me.iamcrk.myfirstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +15,49 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Splash extends AppCompatActivity {
 
+    private TextView splashText;
+
+    private String message = "Welcome to JEE-64";
+
+    private int index = 0;
+
+    private long delay = 150;
+
+    private Handler handler = new Handler(Looper.getMainLooper());
+
+    private Runnable characterAdder = new Runnable() {
+        @Override
+        public void run() {
+            splashText.setText(message.substring(0,index++));
+
+            if (index <= message.length()){
+                handler.postDelayed(characterAdder,delay);
+            }
+            else {
+                handler.postDelayed(() -> {
+                    startActivity(new Intent(getApplicationContext(), Home.class));
+                    finish();
+                },1000);
+            }
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
 
+        splashText = findViewById(R.id.splashText);
 
+        handler.postDelayed(characterAdder,delay);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(characterAdder);
     }
 }
