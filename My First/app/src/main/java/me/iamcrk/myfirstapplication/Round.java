d1afd156639ad2013561051902d8200a4d21a718
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-09-04 22:20:56 +0600

    updated

--- My First/app/src/main/java/me/iamcrk/myfirstapplication/Round.java
+++ My First/app/src/main/java/me/iamcrk/myfirstapplication/Round.java
@@ -23,44 +23,44 @@ public class Round extends AppCompatActivity {
 
         webSubject = findViewById(R.id.webSubject);
 
-        WebSettings webSettings= webSubject.getSettings();
+        WebSettings webSettings = webSubject.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);  // for HTML5 local storage
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
 
         webSubject.setWebChromeClient(new WebChromeClient());
-        webSubject.setWebViewClient(new TopAlignedClient());
-
-        webSubject.setWebViewClient(new SameView());
+        webSubject.setWebViewClient(new SoftLockClient());
 
         webSubject.loadUrl("https://apply.idb-bisew.info/Home/CourseDeatils/13");
-
     }
-    //generated
-    private final class TopAlignedClient extends WebViewClient {
 
-        // Modern override (deprecated String url version avoided)
+    private static String stripFragment(String url) {
+        try {
+            Uri u = Uri.parse(url);
+            return u.buildUpon().fragment(null).build().toString();
+        } catch (Exception e) {
+            return url;
+        }
+    }
+
+    private final class SoftLockClient extends WebViewClient {
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            Uri u = request.getUrl();
-            // If the navigation includes a fragment, reload without it.
-            Uri noFrag = u.buildUpon().fragment(null).build();
-            if (!u.equals(noFrag)) {
-                view.loadUrl(noFrag.toString());
-                return true; // we handled it
-            }
-            return false; // let WebView load normally
+            String cleanUrl = stripFragment(request.getUrl().toString());
+            view.loadUrl(cleanUrl);
+            return true;
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
 
-            // Force scroll to top
+            // Scroll to top immediately
             view.scrollTo(0, 0);
 
-            // Inject JavaScript to kill auto-scroll
+            // Inject JS: prevent site auto-scroll, but allow user scroll
             view.evaluateJavascript(
                     "(function() {" +
                             "   window.scrollTo(0,0);" +
@@ -68,10 +68,10 @@ public class Round extends AppCompatActivity {
                             "   let interval = setInterval(function() {" +
                             "       window.scrollTo(0,0);" +
                             "       tries++;" +
-                            "       if (tries > 20) clearInterval(interval);" + // keep for ~2s
+                            "       if (tries > 3) clearInterval(interval);" + // stop after ~2s//300ms
                             "   }, 100);" +
                             "   document.querySelectorAll('input,textarea,select').forEach(function(el) {" +
-                            "       el.blur();" +  // remove focus so page won't jump
+                            "       el.blur();" + // prevent focus from stealing scroll
                             "   });" +
                             "})();",
                     null
@@ -79,34 +79,4 @@ public class Round extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    public void onBackPressed() {
-//        if (webSubject != null && webSubject.canGoBack()) {
-//            webSubject.goBack();
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
-
-    //generated
-
-    private class SameView extends WebViewClient {
-//        @Override
-//        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//            view.loadUrl(url);
-//            return true;
-//        }
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            view.loadUrl(request.getUrl().toString());
-            return true;
-        }
-
-//        @Override
-//        public void onPageFinished(WebView view, String url) {
-//            super.onPageFinished(view, url);
-//            view.scrollTo(0,0);
-//        }
-    }
 }
